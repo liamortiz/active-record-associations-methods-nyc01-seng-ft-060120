2045b635f300b9813154021e8a99c56f94e21dd5
Leamsi Escribano <leamsii.e@gmail.com>
2020-06-11 23:23:04 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -14,6 +14,6 @@ class Genre < ActiveRecord::Base
 
   def all_artist_names
     # return an array of strings containing every musician's name
-    self.songs.map{|song| song.artist.name}
+    self.songs.artists.map(&:name)
   end
 end
